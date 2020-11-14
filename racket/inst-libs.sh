7200cd64bb6ce66a2146d46141388238731dde97
Your Name <you@example.com>
2020-11-14 14:07:48 +0900

    .

---
+++ racket/inst-libs.sh
@@ -0,0 +1,9 @@
+#! bash -uvx
+PACKAGE_PREFIX="${MINGW_PACKAGE_PREFIX:-mingw-w64-x86_64}"
+echo $PACKAGE_PREFIX
+pacman -S --needed --noconfirm \
+	$PACKAGE_PREFIX-boost \
+	$PACKAGE_PREFIX-fltk \
+	$PACKAGE_PREFIX-libarchive \
+	$PACKAGE_PREFIX-postgresql \
+	$PACKAGE_PREFIX-sqlite3
