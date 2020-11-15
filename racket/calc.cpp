a776fc51b700aff47a1a076e799a65a68ffa3726
Your Name <you@example.com>
2020-11-15 18:25:27 +0900

    .

--- racket/calc.cpp
+++ racket/calc.cpp
@@ -42,10 +42,17 @@ api1_output api1(const api1_input &input)
   //double b = input.b;
   //std::cout << a << std::endl;
   //std::cout << b << std::endl;
-  api1_output result;
-  result.sum = input.a + input.b;
-  result.diff = input.a - input.b;
-  return result;
+  api1_output output;
+  output.sum = input.a + input.b;
+  output.diff = input.a - input.b;
+  time_t t = time(NULL);
+  struct tm tm;
+  char str[81];
+  localtime_s(&tm, &t);
+  strftime(str, sizeof(str), "%Y-%m-%d %H:%I:%S", &tm);
+  printf("%s\n", str);
+  output.ts = str;
+  return output;
 }
 
 double sum(const std::vector<double> &args)
@@ -62,25 +69,31 @@ double sum(const std::vector<double> &args)
 const char *apicall(const char *name, const char *base64_args)
 {
   std::string api_name = name;
-  //std::string result = pack_api_result(0);
   std::string packed_result = pack_result(false);
   std::string packed = base64_decode(std::string(base64_args));
-  msgpack::object_handle oh =
-      msgpack::unpack(packed.data(), packed.size());
-  msgpack::object args = oh.get();
-  std::cout << "API=" << api_name << " args=" << args << std::endl;
+  std::string err;
+  MsgPack obj = MsgPack::parse(packed, err);
+  if (err != "")
+    printf("err=[%s]\n", err.c_str());
+  //std::cout << "API=" << api_name << " args=" << args << std::endl;
   if (api_name == "api1")
   {
-    api1_input input(args);
+    api1_input input(obj);
     api1_output result = api1(input);
-    packed_result = pack_result(result);
+    packed_result = result.pack();
   }
   else if (api_name == "sum")
   {
+    if (!obj.is_array())
+      assert(0);
     std::vector<double> input;
-    args.convert(input);
-    double result = sum(input);
-    packed_result = pack_result(result);
+    auto array = obj.array_items();
+    for (auto it = array.begin(); it != array.end(); ++it)
+    {
+      input.push_back(it->float64_value());
+    }
+    MsgPack result = sum(input);
+    packed_result = result.dump();
   }
   static TLS_VARIABLE_DECL std::string base64_result;
   base64_result = base64_encode(packed_result);
--- racket/calc.hpp
+++ racket/calc.hpp
@@ -1,13 +1,14 @@
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-int add(int x, int y);
-int sub(int x, int y);
-void ref1(int *ret);
-const char* ret_str();
-const char *hello(const char *name);
-const char *apicall(const char *name, const char *base64_args);
+  int add(int x, int y);
+  int sub(int x, int y);
+  void ref1(int *ret);
+  const char *ret_str();
+  const char *hello(const char *name);
+  const char *apicall(const char *name, const char *base64_args);
 
 #ifdef __cplusplus
 } // extern "C" {
@@ -16,6 +17,12 @@ const char *apicall(const char *name, const char *base64_args);
 #ifdef __cplusplus
 
 #include <msgpack.hpp>
+#include "msgpack11.hpp"
+using namespace msgpack11;
+
+#include "msgpack11_dump.hpp"
+
+#include <iostream>
 
 typedef std::map<std::string, msgpack::object> ARGS_AS_MAP;
 typedef std::vector<msgpack::object> ARGS_AS_VECTOR;
@@ -34,6 +41,7 @@ struct api1_input
   double a;
   double b;
   MSGPACK_DEFINE(a, b);
+#if 0x0
   api1_input(const msgpack::object &args)
   {
     ARGS_AS_MAP mmap = args.as<ARGS_AS_MAP>();
@@ -42,12 +50,56 @@ struct api1_input
     this->a = a;
     this->b = b;
   }
+#endif
+  api1_input(const MsgPack &obj)
+  {
+    double a = obj["a"].float64_value();
+    double b = obj["b"].float64_value();
+    this->a = a;
+    this->b = b;
+  }
 };
 struct api1_output
 {
   double sum;
   double diff;
-  MSGPACK_DEFINE(sum, diff);
+  std::string ts;
+  //MSGPACK_DEFINE(sum, diff, ts);
+  std::string pack()
+  {
+    MsgPack obj = MsgPack::object{
+        {"sum", this->sum},
+        {"diff", this->diff},
+        {"ts", this->ts}};
+    return obj.dump();
+  }
+  std::string pack_test()
+  {
+    MsgPack obj = MsgPack::object{
+        {"key1", "value1"},
+        {"key2", false},
+        {"key3", MsgPack::array{1, 2, 3}},
+        {"key4", MsgPack::object{
+                     {"key4-a", 1024},
+                     {"key4-b", MsgPack::binary{0, 2, 4, 6}}}},
+        {"key5", MsgPack::array{MsgPack::object{{"key5-1-a", 100}, {"key5-1-b", 200}}, MsgPack::object{{"key5-2-a", 300}, {"key5-2-b", 400}}}}};
+    std::cout << "---- original ----" << std::endl;
+    show(obj);
+    std::cout << obj["key1"].string_value() << std::endl;
+    std::cout << obj["key2"].string_value() << std::endl;
+    std::cout << obj["key2"].bool_value() << std::endl;
+    show(obj["key3"]);
+    std::cout << obj["key3"].array_items().size() << std::endl;
+    std::cout << obj["key3"].array_items()[2].int64_value() << std::endl;
+    std::cout << obj["key3"].array_items()[2].type() << std::endl;
+    std::string err;
+    MsgPack serdes_obj = MsgPack::parse(obj.dump(), err);
+    std::cout << "---- ser/des result ----" << std::endl;
+    show(serdes_obj);
+    MsgPack obj2 = MsgPack::array{110, 220, 330};
+    show(obj2);
+    return "";
+  }
 };
 api1_output api1(const api1_input &input);
 
