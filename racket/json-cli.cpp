f98e129ad86c348713903e64e5730b273f7ea5d1
Your Name <you@example.com>
2020-11-16 20:12:08 +0900

    .

--- racket/json-cli.cpp
+++ racket/json-cli.cpp
@@ -20,7 +20,7 @@ namespace ns
         std::string address;
         int age;
     };
-}
+} // namespace ns
 
 namespace ns
 {
@@ -35,7 +35,7 @@ namespace ns
         j.at("address").get_to(p.address);
         j.at("age").get_to(p.age);
     }
-}
+} // namespace ns
 
 json api_persons(/*const json &input*/)
 {
@@ -51,11 +51,12 @@ json api_persons(/*const json &input*/)
 int handle_pipe_events(const std::string &pipe_name)
 {
     HANDLE hPipe = create_pipe_client(pipe_name);
-    while(true)
+    while (true)
     {
         std::string input_json = read_string_from_pipe(hPipe);
         std::cout << "input_json=" << input_json << std::endl;
-        if(input_json.empty()) break;
+        if (input_json.empty())
+            break;
         json packet = json::parse(input_json);
         std::string api_name = packet["name"];
         json value = packet["value"];
@@ -67,14 +68,37 @@ int handle_pipe_events(const std::string &pipe_name)
     return 0;
 }
 
-std::string unsigned_to_string(unsigned long long n)
+static inline std::string unsigned_to_string(unsigned long long n)
 {
     return std::to_string(n);
 }
 
-unsigned long long string_to_unsigned(const std::string &s)
+static inline unsigned long long string_to_unsigned(const std::string &s)
 {
     return std::stoull(s);
+}
+
+#if 0x1
+static inline void *unsigned_to_address(unsigned long long n)
+{
+    return (void *)n;
+}
+
+static inline unsigned long long address_to_unsigned(void *p)
+{
+    return (unsigned long long)p;
+}
+#endif
+
+static inline std::string address_to_string(void *p)
+{
+    unsigned long long n = (unsigned long long)p;
+    return std::to_string(n);
+}
+
+static inline void *string_to_address(const std::string &s)
+{
+    return (void *)std::stoull(s);
 }
 
 extern "C" void __wgetmainargs(int *, wchar_t ***, wchar_t ***, int, int *);
@@ -84,8 +108,9 @@ int main(int, char **)
     wchar_t **argv, **env;
     __wgetmainargs(&argc, &argv, &env, 0, &si);
     printf("argc=%d\n", argc);
-    if(argc >= 2) {
-        std::string pipe_name = wide_to_utf8(argv[1]).c_str(); 
+    if (argc >= 2)
+    {
+        std::string pipe_name = wide_to_utf8(argv[1]).c_str();
         printf("pipe_name=%s\n", pipe_name.c_str());
         return handle_pipe_events(pipe_name);
     }
@@ -103,17 +128,22 @@ int main(int, char **)
     json output = api_persons();
     std::cout << output << std::endl;
 
-#if 0x1
     auto n = 18446744073709551615ULL;
     std::string s = unsigned_to_string(n);
     std::cout << s << std::endl;
-#else
-    printf("n=%llu\n", 18446744073709551615);
-#endif
-    //unsigned long long ull = std::stoull(s);
     unsigned long long ull = string_to_unsigned(s);
     std::cout << ull << std::endl;
 
+    void *addr = unsigned_to_address(ull);
+    auto ull2 = address_to_unsigned(addr);
+    std::cout << ull2 << std::endl;
+
+    std::cout << address_to_string(addr) << std::endl;
+
+    void *addr2 = string_to_address(s);
+    std::cout << address_to_string(addr2) << std::endl;
+
+    HANDLE hdl = addr2;
 
     return 0;
 }
