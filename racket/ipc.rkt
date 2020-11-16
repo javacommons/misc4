88f2541992917badebf24de925ac6523a4fd3e09
Your Name <you@example.com>
2020-11-16 18:42:22 +0900

    .

--- racket/ipc.rkt
+++ racket/ipc.rkt
@@ -3,8 +3,8 @@
 (require gregor)
 (require ffi/unsafe
          ffi/unsafe/define)
-(require msgpack)
-(require net/base64)
+;(require msgpack)
+;(require net/base64)
 (require json)
 
 (define-ffi-definer define-calc (ffi-lib "calc"))
@@ -13,33 +13,22 @@
 (define-calc read_from_pipe(_fun _ullong -> _string))
 (define-calc write_to_pipe(_fun _ullong _string -> _void))
 
-;; (define (encode-input $name $value)
-;;   (let* ([$input (hash "name" $name "value" $value)]
-;; 	 [$packed-input (pack $input)]
-;; 	 [$base64-input (base64-encode $packed-input #"")])
-;;     $base64-input))
 (define (encode-input $name $value)
   (let* ([$input (hasheq 'name $name 'value $value)]
 	 [$json (jsexpr->string $input)])
     $json))
 
-;; (define (decode-output $base64)
-;;   (let* ([$packed-output (base64-decode $base64)]
-;; 	 [$output (unpack $packed-output)])
-;;     $output))
 (define (decode-output $json)
   (let* ([$output (string->jsexpr $json)])
     $output))
 
-(define (decode-input $base64)
-  (let* ([$packed-input (base64-decode $base64)]
-	 [$input (unpack $packed-input)])
+(define (decode-input $json)
+  (let* ([$input (string->jsexpr $json)])
     $input))
 
 (define (encode-output $output)
-  (let* ([$packed-output (pack $output)]
-	 [$base64-output (base64-encode $packed-output)])
-    $base64-output))
+  (let* ([$json (jsexpr->string $output)])
+    $json))
 
 (define (::open-pipe-server $name $client $show-client)
   (open_pipe_server $name $client $show-client))
@@ -49,9 +38,7 @@
 
 (define (::call-thru-pipe $hPipe $name $value)
   (write_to_pipe $hPipe (encode-input $name $value))
-  ;(decode-output (string->bytes/latin-1 (read_from_pipe $hPipe)))
-  (decode-output (read_from_pipe $hPipe))
-  )
+  (decode-output (read_from_pipe $hPipe)))
 
 (define (::receive-input-thru-pipe $hPipe)
   (let* ([$base64 (read_from_pipe $hPipe)])
