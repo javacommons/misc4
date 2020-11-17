4bf743c9932867ca1efced9cfd4538e0d66b4fa1
Your Name <you@example.com>
2020-11-17 20:54:25 +0900

    .

--- racket/unzip.rkt
+++ racket/unzip.rkt
@@ -6,12 +6,12 @@
 (define prepare-dir%
   (class
    object%
-   (init root-dir zip-path)
+   (init :root-dir :zip-path)
    (super-new)
-   (println root-dir)
-   (define %target-dir (build-path $home-dir ".software/racket-7.9-64bit"))
+   (println :root-dir)
+   (define %target-dir (build-path :root-dir ".software/racket-7.9-64bit"))
    (println %target-dir)
-   (define %file-in (open-input-file "racket-7.9-64bit.zip" #:mode 'binary))
+   (define %file-in (open-input-file :zip-path #:mode 'binary))
    (println %file-in)
    (cond
     [(not (directory-exists? %target-dir))
@@ -30,8 +30,8 @@
 $home-dir
 
 (define $pd (new prepare-dir%
-		 [root-dir $home-dir]
-		 [zip-path "racket-7.9-64bit.zip"]))
+		 [:root-dir $home-dir]
+		 [:zip-path "racket-7.9-64bit.zip"]))
 (define $target-dir (send $pd result))
 $target-dir
 
@@ -63,3 +63,26 @@ $path-list
 $path-list
 (if (member $target-str $path-list) (println "<is member>") #f)
 (string-join $path-list ";")
+
+(define set-path%
+  (class
+   object% (super-new)
+   (init :member)
+   (define %member (if (path? :member) (path->string :member) :member))
+   (println %member)
+   (set! %member (string-replace %member "/" "\\"))
+   (println %member)
+   (define %path (get-registry-env "USER" "PATH"))
+   (println %path)
+   (define %path-list (string-split $path ";"))
+   (println %path-list)
+   (set! %path-list (cons %member %path-list))
+   (set! %path-list (remove-duplicates %path-list equal?))
+   (println %path-list)
+   (set! %path (string-join %path-list ";"))
+   (println %path)
+   (set-registry-env! "USER" "PATH" %path)
+   )
+  )
+
+(new set-path% [:member $target-dir])
