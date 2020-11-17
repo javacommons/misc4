5db96b7afb5321df53f2886665b4bb15417d9c9c
Your Name <you@example.com>
2020-11-18 01:05:56 +0900

    .

--- racket/run-file.rkt
+++ racket/run-file.rkt
@@ -9,21 +9,3 @@ $abs-path
 
 (define $home-dir (find-system-path 'home-dir))
 $home-dir
-
-(define prepare-dir%
-  (class
-   object%
-   (init root-dir)
-   (super-new)
-   (println root-dir)
-   (define %target-dir (build-path $home-dir ".software/racket-7.9-64bit"))
-   (println %target-dir)
-   (make-directory* %target-dir)
-   (define/public (result)
-     %target-dir)
-   )
-  )
-
-(define $pd (new prepare-dir% [root-dir $home-dir]))
-(send $pd result)
-
