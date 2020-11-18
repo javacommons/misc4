ce846f4507db8ad7b2a8ff01d3f5dd908eadd442
Your Name <you@example.com>
2020-11-18 09:25:50 +0900

    .

--- racket/VSCode-win32-x64-1.51.1.rkt
+++ racket/VSCode-win32-x64-1.51.1.rkt
@@ -17,3 +17,15 @@ $target-dir
 (define $bin-dir (build-path $target-dir "bin"))
 
 (new set-path% [:member $bin-dir])
+
+;(define $home-dir (find-system-path 'home-dir))
+$home-dir
+
+(define $sendto-dir (build-path $home-dir "AppData\\Roaming\\Microsoft\\Windows\\SendTo"))
+$sendto-dir
+
+(define $shortcut-path (path->string (build-path $sendto-dir "VSCode.lnk")))
+(define $target-path (path->string (build-path $target-dir "Code.exe")))
+(new create-simple-shortcut%
+     [:shortcut-path $shortcut-path]
+     [:target-path $target-path])
