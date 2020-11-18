3b18b6b1b425b50542713f93b123295c41747f90
Your Name <you@example.com>
2020-11-18 18:00:41 +0900

    .

---
+++ racket/cmdline.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+;; parameter My-Name is one of:
+;; - #false
+;; - String
+(define my-name (make-parameter #false))
+
+;; command line parser
+(define parser
+  (command-line
+   #:usage-help
+   "Have the computer greet you!"
+   "Have the computer greet you!"
+
+   #:once-each
+   [("-n" "--name") NAME
+                    "Set your name"
+                    (my-name NAME)]
+
+   #:args (FILENAME)
+   ;(void)
+   FILENAME
+   ))
+
+;; get-greeting : My-Name -> String
+;; Gets the greeting for the given My-Name
+(define (get-greeting mn)
+  (cond
+    [(boolean? mn) "Hello, unknown person!"]
+    [(string? mn) (string-append "Hello, " mn "!")]))
+
+;; prints result to the command line
+(printf "~a\n" (get-greeting (my-name)))
+(my-name)
+(void)
+parser
