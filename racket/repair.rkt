3ced386bc6286f8e046141d3d76b00b1b74b14e7
Your Name <you@example.com>
2020-11-19 13:31:20 +0900

    .

--- racket/repair.rkt
+++ racket/repair.rkt
@@ -2,6 +2,8 @@
 
 (require "zip-install.rkt")
 
+(get-registry-env "USER" "xyz")
+
 (define $path (get-registry-env "USER" "PATH"))
 $path
 
@@ -17,6 +19,19 @@ $path-list
 
 (filter is-msys64 $path-list)
 (filter not-msys64 $path-list)
+
+(require ffi/com)
+
+(define (delete-registry-env %scope %name)
+  (let* ([%wshell (com-create-instance "WScript.Shell" 'local)]
+         [%env (com-get-property* %wshell "Environment" %scope)]
+         [%tem1 (println(com-methods %env))]
+         [%result (com-get-property %env (list "Item" %name))])
+    (if (equal? "" %result) (void) (com-invoke %env "Remove" %name))
+    (com-release %wshell)
+    %result))
+
+(delete-registry-env "USER" "MSYSTEM_CARCH")
 
 #;(define set-path%
   (class
