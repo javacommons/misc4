0bba9b2cf9d9fe99ba4d5ea885c7c355fd718e0b
Your Name <you@example.com>
2020-11-19 11:25:08 +0900

    .

---
+++ racket/bash64.cmd
@@ -0,0 +1,11 @@
+setlocal
+set PATH=PATH=C:\Users\user\.software\msys64\mingw64\bin;C:\Users\user\.software\msys64\usr\local\bin;C:\Users\user\.software\msys64\usr\bin;%PATH%
+set MINGW_CHOST=x86_64-w64-mingw32
+set MINGW_PACKAGE_PREFIX=mingw-w64-x86_64
+set MINGW_PREFIX=/mingw64
+set MSYSTEM_CARCH=x86_64
+set MSYSTEM_CHOST=x86_64-w64-mingw32
+set MSYSTEM_PREFIX=/mingw64
+set MSYSTEM=MINGW64
+bash.exe
+endlocal
